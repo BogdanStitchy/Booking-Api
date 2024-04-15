f62449475a966460049aa7729f64e086fae61e7f
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-15 11:53:16 +0300

    work with exceptions is placed in a separate file

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-
-from fastapi import HTTPException, status, Request, Depends
+from fastapi import Request, Depends
 from jose import jwt, JWTError
 
+from app.exceptions import TokenExpiredException, TokenAbsentException, IncorrectTokenFormatException, \
+    UserIsNotPresentException
 from app.users.dao import UsersDAO
 from config import config
 
@@ -10,7 +11,7 @@ from config import config
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenAbsentException
     return token
 
 
@@ -20,18 +21,18 @@ async def get_current_user(token: str = Depends(get_token)):
             token, config.SECRET_KEY, config.HASH_FUNCTION
         )
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectTokenFormatException
 
     expire: str = payload.get("exp")
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenExpiredException
 
     user_id: str = payload.get("sub")
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
 
     user = await UsersDAO.find_by_id(int(user_id))
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
 
     return user
--- app/users/router.py
+++ app/users/router.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, HTTPException, status, Response
+from fastapi import APIRouter, Response, Depends
 
+from app.exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException
 from app.users.dao import UsersDAO
+from app.users.models import Users
 from app.users.shemas import SUserAuth
 from app.users.auth import get_password_hash, create_access_token, authenticate_user
+from app.users.dependencies import get_current_user
 
 router = APIRouter(
     prefix="/auth",
@@ -14,7 +17,7 @@ router = APIRouter(
 async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
-        raise HTTPException(status_code=500)
+        raise UserAlreadyExistsException
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
 
@@ -23,7 +26,7 @@ async def register_user(user_data: SUserAuth):
 async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectEmailOrPasswordException
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return access_token
@@ -32,3 +35,8 @@ async def login_user(response: Response, user_data: SUserAuth):
 @router.post("/logout")
 async def logout_user(response: Response):
     response.delete_cookie("booking_access_token")
+
+
+@router.post("/me")
+async def read_users_me(response: Response, current_user: Users = Depends(get_current_user)):
+    return current_user
