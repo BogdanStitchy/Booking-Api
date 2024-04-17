c8678a75c7bcede6c1de4c1eaeea4b2435fab954
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-17 19:28:57 +0300

    Added router to work with Jinja templates. This router is connected. Created an endpoint for loading hotel covers into the database. Template for displaying hotels updated to work with image covers

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
@@ -7,14 +8,19 @@ from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 
 from app.pages.router import router as router_pages
+from app.images.router import router as router_images
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+
 app.include_router(router_pages)
+app.include_router(router_images)
 
 
 class SHotel(BaseModel):
