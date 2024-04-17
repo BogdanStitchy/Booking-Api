b189a91d7313f1b99f0cacdf0721ae13cbbffb17
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-17 13:41:15 +0300

    added working with Jinja templates

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI, Query
-from typing import Union
-from datetime import date
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
@@ -8,12 +6,15 @@ from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 
+from app.pages.router import router as router_pages
+
 app = FastAPI()
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+app.include_router(router_pages)
 
 
 class SHotel(BaseModel):
