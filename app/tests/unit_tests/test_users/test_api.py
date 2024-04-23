0da540ee4abf498f33189d6329d17b01f3abfc74
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-23 21:09:56 +0300

    tests for registration and user authentication were created

--- app/tests/unit_tests/test_users/test_api.py
+++ app/tests/unit_tests/test_users/test_api.py
@@ -1,5 +1,32 @@
 import pytest
+from httpx import AsyncClient
 
 
-async def test_abc():
-    assert 1 == 1
+# async_client
+# post - json
+# get - params
+@pytest.mark.parametrize("email, password, status_code", [
+    ("Ivan@van.com", "Ivan", 200),
+    ("Ivan@van.com", "Ivanovich", 409),
+    ("adcde", "Ivanovich", 422),
+])
+async def test_register_user(async_client: AsyncClient, email, password, status_code):
+    response = await async_client.post("auth/register", json={
+        "email": email,
+        "password": password
+    })
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("ivan@test.com", "pass", 200),
+    ("test@test.com", "_---_", 401),
+    ("test@test.com", "pass", 200),
+    ("t@test.com", "pass", 401),
+])
+async def test_login_user(async_client: AsyncClient, email, password, status_code):
+    response = await async_client.post("auth/login", json={
+        "email": email,
+        "password": password
+    })
+    assert response.status_code == status_code
