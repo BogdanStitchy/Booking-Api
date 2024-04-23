f02b329c24e62ff19e750079311da0aa1e9fc552
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-23 21:06:52 +0300

    fixed error authenticate

--- app/dao/base.py
+++ app/dao/base.py
@@ -15,9 +15,9 @@ class BaseDAO:
     @classmethod
     async def find_one_or_none(cls, **filer_by):
         async with async_session_maker() as session:
-            query = select(cls.model).filter_by(**filer_by)
+            query = select(cls.model.__table__).filter_by(**filer_by)
             result = await session.execute(query)
-            return result.scalar_one_or_none()
+            return result.mappings().first()
 
     @classmethod
     async def get_all(cls, **filer_by):
