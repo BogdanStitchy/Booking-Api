f79aae5624235c8e9dc818074103ad8820970702
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-24 21:11:36 +0300

    added parameterization of test_add_and_get_booking test. Added test of getting bookings

--- app/tests/integration_tests/test_bookings/test_dao_bookings.py
+++ app/tests/integration_tests/test_bookings/test_dao_bookings.py
@@ -1,15 +1,37 @@
-from datetime import datetime
+from datetime import datetime, date
+import pytest
 from app.bookings.dao import BookingDAO
 
 
-async def test_add_and_get_booking():
+@pytest.mark.parametrize("expected_id", [
+    4, 5, 6
+])
+async def test_add_and_get_booking(expected_id):
     new_booking_id = await BookingDAO.add(
         user_id=2,
         room_id=2,
         date_from=datetime.strptime("2024-06-10", "%Y-%m-%d"),
         date_to=datetime.strptime("2024-06-20", "%Y-%m-%d")
     )
-    assert new_booking_id == 4
+    assert new_booking_id == expected_id
 
     new_booking = await BookingDAO.find_by_id(new_booking_id)
     assert new_booking is not None
+
+
+@pytest.mark.parametrize("id_booking, expected_data_booking", [
+    (1, {'id': 1, 'room_id': 1, 'user_id': 1, 'date_from': date(2023, 6, 15),
+         'date_to': date(2023, 6, 30), 'price': 24500, 'total_cost': 367500, 'total_days': 15,
+         'room_name': 'Улучшенный с террасой и видом на озеро', 'hotel_name': 'Cosmos Collection Altay Resort',
+         'location': 'Республика Алтай, Майминский район, село Урлу-Аспак, Лесхозная улица, 20'}),
+
+    (2, {'id': 2, 'room_id': 2, 'user_id': 1, 'date_from': date(2023, 4, 1),
+         'date_to': date(2023, 4, 19), 'price': 4300, 'total_cost': 77400, 'total_days': 18,
+         'room_name': 'Делюкс Плюс', 'hotel_name': 'Cosmos Collection Altay Resort',
+         'location': 'Республика Алтай, Майминский район, село Урлу-Аспак, Лесхозная улица, 20'})
+])
+async def test_get_bookings(id_booking, expected_data_booking):
+    booking = await BookingDAO.get_bookings_data_for_email(id_booking)
+    print(f"{booking=}")
+    print(f"{type(booking)=}")
+    assert booking == expected_data_booking
