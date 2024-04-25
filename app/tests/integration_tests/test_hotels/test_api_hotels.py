7f286f25c11a20e6c553c93d15a7e3678f7e19eb
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-04-25 11:09:06 +0300

    added api test for getting one hotels

--- app/tests/integration_tests/test_hotels/test_api_hotels.py
+++ app/tests/integration_tests/test_hotels/test_api_hotels.py
@@ -14,3 +14,25 @@ async def test_api_get_hotels(async_client, start_redis_for_method_with_cache,
     })
 
     assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("hotel_id, status_code, expected_data", [
+    (1, 200,
+     {'id': 1, 'name': 'Cosmos Collection Altay Resort',
+      'location': 'Республика Алтай, Майминский район, село Урлу-Аспак, Лесхозная улица, 20',
+      'services': ['Wi-Fi', 'Бассейн', 'Парковка', 'Кондиционер в номере'], 'rooms_quantity': 15,
+      'image_id': 1}),
+
+    (2, 200,
+     {'id': 2, 'name': 'Skala', 'location': 'Республика Алтай, Майминский район, поселок Барангол, Чуйская улица 40а',
+      'services': ['Wi-Fi', 'Парковка'], 'rooms_quantity': 23, 'image_id': 2}),
+
+    (12, 404, {}),
+])
+async def test_api_get_one_hotel(async_client, start_redis_for_method_with_cache,
+                                 hotel_id, status_code, expected_data):
+    response = await async_client.get(f"/hotels/{hotel_id}/1")
+    if status_code == 200:
+        assert response.json() == expected_data
+
+    assert response.status_code == status_code
